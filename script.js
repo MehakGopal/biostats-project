3e2cc4c0191c46eda939928f268ed78f951b919f
MehakGopal <108086780+MehakGopal@users.noreply.github.com>
2024-04-07 13:50:17 +0530

    bobblehead and speech

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var questionAnswers = [];
-
-
+var facts = [['Hypothesis Testing refers to an objective method of making decisions or inferences from sample data.'],['Parametric tests are used for data that show a normal distribution'],['Parametric tests are used for data that are independent of one another'],['Parametric tests are used for data that are on the same continous scale of measurement.'],['The prevalence of a disease is the probability of currently having the disease regardless of the duration of time one has had the disease'],['The cumulative incidence of a disease is the probability that a person with no prior disease will develop a new case of the disease over some specified time period.']]
+var bob = ["bobblehead-container-1","bobblehead-container-2","bobblehead-container-3","bobblehead-container-4"]
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function getTextOfButton(buttonId) {
   if(buttonId.match(/yes/i)){
     return 'Yes';
@@ -50,12 +53,16 @@ function nextQuestion(option) {
   var answer = getTextOfButton(option); // Get the text of the button
   questionAnswers.push({ question: currentQuestion, answer: answer });
   console.log(questionAnswers);
+  var bob_id = getRandomInt(0,bob.length-1);
+  var fact = getRandomInt(0,facts.length-1);
   switch (option) {
       case 'oneplus':
       questionContainer.innerHTML = `
           <h2 id="question">Are you interested in the relationship between the variables?</h2>
           <button onclick="nextQuestion('yes1')">Yes</button>
           <button onclick="nextQuestion('no1')">No</button>`;
+        // Example usage:
+      addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'one':
       questionContainer.innerHTML = `
@@ -63,24 +70,28 @@ function nextQuestion(option) {
           <button onclick="nextQuestion('1s')">One sample</button>
           <button onclick="nextQuestion('2s')">Two sample</button>
           <button onclick="nextQuestion('2pluss')">More than 2 samples</button`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case '1s':
         questionContainer.innerHTML = `
           <h2 id="question">Is the underlying distribution normal or can CLT be assumed?</h2>
           <button onclick="nextQuestion('yes18')">Yes</button>
           <button onclick="nextQuestion('no18')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes18':
         questionContainer.innerHTML = `
           <h2 id="question">Inference concerning?</h2>
           <button onclick="nextQuestion('yes19')">Yes</button>
           <button onclick="nextQuestion('no19')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes19':
         questionContainer.innerHTML = `
           <h2 id="question">Known?</h2>
           <button onclick="nextQuestion('yes20')">Yes</button>
           <button onclick="nextQuestion('no20')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes20':
         displayTextAndConfetti("One Sample Z test",1000);
@@ -99,6 +110,7 @@ function nextQuestion(option) {
           <h2 id="question">Is the underlying distribution binomial?</h2>
           <button onclick="nextQuestion('yes21')">Yes</button>
           <button onclick="nextQuestion('no21')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes21':
         displayTextAndConfetti("One sample binomial test",500)
@@ -106,6 +118,7 @@ function nextQuestion(option) {
           <h2 id="question">Normal Approximation valid?</h2>
           <button onclick="nextQuestion('yes22')">Yes</button>
           <button onclick="nextQuestion('no22')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes22':
         displayTextAndConfetti("Normal Theory Methods",1000);
@@ -120,6 +133,7 @@ function nextQuestion(option) {
           <h2 id="question">Underlying distribution is poisson?</h2>
           <button onclick="nextQuestion('yes23')">Yes</button>
           <button onclick="nextQuestion('no23')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes23':
         displayTextAndConfetti("One Sample Poisson Test",1000);
@@ -134,24 +148,28 @@ function nextQuestion(option) {
           <h2 id="question">Underlying Distribution normal or can CLT be assumed?</h2>
           <button onclick="nextQuestion('yes24')">Yes</button>
           <button onclick="nextQuestion('no24')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes24':
         questionContainer.innerHTML = `
           <h2 id="question">Inference concerning means?</h2>
           <button onclick="nextQuestion('yes25')">Yes</button>
           <button onclick="nextQuestion('no25')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes25':
         questionContainer.innerHTML = `
           <h2 id="question">Are samples independent?</h2>
           <button onclick="nextQuestion('yes26')">Yes</button>
           <button onclick="nextQuestion('no26')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes26':
         questionContainer.innerHTML = `
           <h2 id="question">Are variances of two samples significantly different?</h2>
           <button onclick="nextQuestion('yes27')">Yes</button>
           <button onclick="nextQuestion('no27')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes27':
         displayTextAndConfetti("Use two sample t-test with unequal variances",1000);
@@ -174,30 +192,35 @@ function nextQuestion(option) {
           <h2 id="question">Underlying distribution normal?</h2>
           <button onclick="nextQuestion('yes28')">Yes</button>
           <button onclick="nextQuestion('no28')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes28':
         questionContainer.innerHTML = `
           <h2 id="question">Are samples independent?</h2>
           <button onclick="nextQuestion('yes29')">Yes</button>
           <button onclick="nextQuestion('no29')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes29':
         questionContainer.innerHTML = `
           <h2 id="question">Are all expected values>=5?</h2>
           <button onclick="nextQuestion('yes30')">Yes</button>
           <button onclick="nextQuestion('no30')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes30':
         questionContainer.innerHTML = `
           <h2 id="question">2x2 Contingency table?</h2>
           <button onclick="nextQuestion('yes32')">Yes</button>
           <button onclick="nextQuestion('no32')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'no32':
         questionContainer.innerHTML = `
           <h2 id="question">2xk Contingency table?</h2>
           <button onclick="nextQuestion('yes33')">Yes</button>
           <button onclick="nextQuestion('no33')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
       case 'no33':
         displayTextAndConfetti("Use Chi-Square test for RxC tables",1000);
@@ -208,6 +231,7 @@ function nextQuestion(option) {
           <h2 id="question">Interested in a trend over k binomial proportions?</h2>
           <button onclick="nextQuestion('yes34')">Yes</button>
           <button onclick="nextQuestion('no34')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
       case 'yes34':
         displayTextAndConfetti("Use chi-square test for trend, if no confounding is present; or the Mantel-Haenszel method, if confounding is present",1000);
@@ -234,6 +258,7 @@ function nextQuestion(option) {
           <h2 id="question">Person Time data?</h2>
           <button onclick="nextQuestion('yes3')">Yes</button>
           <button onclick="nextQuestion('no31')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'no31':
         displayTextAndConfetti("Use another underlying distribution or use non-parametric methods",1000);
@@ -244,6 +269,7 @@ function nextQuestion(option) {
           <h2 id="question">Underlying distribution normal or can CLT be assumed?</h2>
           <button onclick="nextQuestion('yes35')">Yes</button>
           <button onclick="nextQuestion('no35')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
       case 'yes35':
         displayTextAndConfetti("One way ANOVA",1000);
@@ -254,6 +280,7 @@ function nextQuestion(option) {
           <h2 id="question">Categorical Data?</h2>
           <button onclick="nextQuestion('yes36')">Yes</button>
           <button onclick="nextQuestion('no36')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
       case 'yes36':
         displayTextAndConfetti("Use RxC contingency table methods",1000);
@@ -268,12 +295,14 @@ function nextQuestion(option) {
           <h2 id="question">Are both the variables continuous?</h2>
           <button onclick="nextQuestion('yes9')">Yes</button>
           <button onclick="nextQuestion('no9')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
       case 'yes9':
         questionContainer.innerHTML = `
           <h2 id="question">Are you interested in predicting one variable from another?</h2>
           <button onclick="nextQuestion('yes10')">Yes</button>
           <button onclick="nextQuestion('no10')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
       case 'yes10':
         displayTextAndConfetti("Simple Linear Regression",1000);
@@ -284,6 +313,7 @@ function nextQuestion(option) {
           <h2 id="question">Are both the variables normal?</h2>
           <button onclick="nextQuestion('yes11')">Yes</button>
           <button onclick="nextQuestion('no11')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
       case 'yes11':
         displayTextAndConfetti("Pearson Correlation Methods",1000);
@@ -298,6 +328,7 @@ function nextQuestion(option) {
           <h2 id="question">Is one variable continuous and one categorical?</h2>
           <button onclick="nextQuestion('yes12')">Yes</button>
           <button onclick="nextQuestion('no12')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
       case 'yes12':
         displayTextAndConfetti("ANOVA",500);
@@ -306,18 +337,21 @@ function nextQuestion(option) {
           <button onclick="nextQuestion('1')">1</button>
           <button onclick="nextQuestion('2')">2</button>
           <button onclick="nextQuestion('2plus')">More than 2</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
       case '1':
         questionContainer.innerHTML = `
         <h2 id="question">Is outcome variable normal or can CLT be assumed?</h2>
         <button onclick="nextQuestion('yes13')">Yes</button>
         <button onclick="nextQuestion('no13')">No</button>`;
+        addSpeechBubble(bob[bob_id],facts[fact]);
         break;
       case 'yes13':
         questionContainer.innerHTML = `
         <h2 id="question">Other covariates to be controlled for?</h2>
         <button onclick="nextQuestion('yes14')">Yes</button>
         <button onclick="nextQuestion('no14')">No</button>`;
+        addSpeechBubble(bob[bob_id],facts[fact]);
         break;
       case 'yes14':
         displayTextAndConfetti("One Way ANCOVA",1000);
@@ -336,6 +370,7 @@ function nextQuestion(option) {
         <h2 id="question">Other covariates to be controlled for?</h2>
         <button onclick="nextQuestion('yes15')">Yes</button>
         <button onclick="nextQuestion('no15')">No</button>`;
+        addSpeechBubble(bob[bob_id],facts[fact]);
         break;
       case 'yes15':
         displayTextAndConfetti("Two Way ANCOVA",1000);
@@ -350,6 +385,7 @@ function nextQuestion(option) {
         <h2 id="question">Other covariates to be controlled for?</h2>
         <button onclick="nextQuestion('yes16')">Yes</button>
         <button onclick="nextQuestion('no16')">No</button>`;
+        addSpeechBubble(bob[bob_id],facts[fact]);
         break;
       case 'yes16':
         displayTextAndConfetti("Higher Way ANCOVA",1000);
@@ -364,6 +400,7 @@ function nextQuestion(option) {
           <h2 id="question">Is the data Ordinal?</h2>
           <button onclick="nextQuestion('yes17')">Yes</button>
           <button onclick="nextQuestion('no17')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'yes17':
         displayTextAndConfetti("Rank-Correlation Methods",1000);
@@ -374,6 +411,7 @@ function nextQuestion(option) {
           <h2 id="question">Interested in test of association or reproducibility?</h2>
           <button onclick="nextQuestion('associ')">Association</button>
           <button onclick="nextQuestion('repro')">Reproducibility</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'associ':
         displayTextAndConfetti("Use contingency table methods",1000);
@@ -388,6 +426,7 @@ function nextQuestion(option) {
           <h2 id="question">Is the outcome variable continous or binary?</h2>
           <button onclick="nextQuestion('conti')">Continuous</button>
           <button onclick="nextQuestion('binary')">Binary</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
       break;
       case 'conti':
         displayTextAndConfetti("Multiple Regression Methods!",400)
@@ -398,6 +437,7 @@ function nextQuestion(option) {
           <h2 id="question">Are time of events important?</h2>
           <button onclick="nextQuestion('yes3')">Yes</button>
           <button onclick="nextQuestion('no3')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
         break;
         case 'no3':
           displayTextAndConfetti("Multiple Logistic Regression Methods!",400)
@@ -409,6 +449,7 @@ function nextQuestion(option) {
           <h2 id="question">Is it a one sample problem</h2>
           <button onclick="nextQuestion('yes4')">Yes</button>
           <button onclick="nextQuestion('no4')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
         case 'yes4':
           displayTextAndConfetti("One sample-test for Incidence rates",400)
@@ -419,18 +460,21 @@ function nextQuestion(option) {
           <h2 id="question">Are the incident rates constant over time?</h2>
           <button onclick="nextQuestion('yes5')">Yes</button>
           <button onclick="nextQuestion('no5')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
         case 'yes5':
           questionContainer.innerHTML = `
           <h2 id="question">Is it a two sample problem?</h2>
           <button onclick="nextQuestion('yes6')">Yes</button>
           <button onclick="nextQuestion('no6')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
         case 'no5':
           questionContainer.innerHTML = `
           <h2 id="question">Interested in comparison of survival curves of two groups with limited control of variates?</h2>
           <button onclick="nextQuestion('yes7')">Yes</button>
           <button onclick="nextQuestion('no7')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
         case 'yes7':
           displayTextAndConfetti("Log Rank Test",800);
@@ -441,6 +485,7 @@ function nextQuestion(option) {
           <h2 id="question">Willing to assume several curve comes from a weibul distribution?</h2>
           <button onclick="nextQuestion('yes8')">Yes</button>
           <button onclick="nextQuestion('no8')">No</button>`;
+          addSpeechBubble(bob[bob_id],facts[fact]);
           break;
         case 'yes8':
           displayTextAndConfetti("Use parameter survival methods based on weibul distribution",1000)
@@ -534,3 +579,38 @@ function navigateToNewPage() {
   // Redirect to the new page
   window.location.href = url;
 }
+function clear(){
+  for(var i = 0;i<bob.length;i++){
+    var parentDiv = document.getElementById(bob[i]);
+    parentDiv.innerHTML='';
+  }
+}
+function addSpeechBubble(parentId,text) {
+  clear();
+  var parentDiv = document.getElementById(parentId);
+  var speechBubbleDiv = document.createElement('div');
+  speechBubbleDiv.classList.add('speech-bubble');
+  var iconDiv = document.createElement('div');
+  iconDiv.classList.add('speech-bubble-icon');
+  var iconElement = document.createElement('i');
+  iconElement.classList.add('fa', 'fa-2x', 'fa-gittip');
+  iconDiv.appendChild(iconElement);  
+  var textDiv = document.createElement('div');
+  textDiv.classList.add('speech-bubble-text');  
+  // Create and append text lines
+  var textLines = text;
+  textLines.forEach(function(lineText) {
+      var line = document.createElement('p');
+      line.classList.add('speech-bubble-text-line');
+      line.textContent = lineText;
+      textDiv.appendChild(line);
+  });  
+  speechBubbleDiv.appendChild(iconDiv);
+  speechBubbleDiv.appendChild(textDiv);
+  
+  // Append the speech bubble to the parent div
+  parentDiv.appendChild(speechBubbleDiv);
+}
+
+
+
